6dc1ad51639c845f0b51505defd7983d1ac34fe1
Ruochen Wu <97808991+Rc-W024@users.noreply.github.com>
2025-05-14 17:45:16 +0200

    Update main.m

--- main.m
+++ main.m
@@ -234,10 +234,11 @@ blp_r=fir1(300,cutoff_f/(smp_f/2),'low');
 rsig_lp=filtfilt(blp_r,1,vitsig);
 hsig_lp=vitsig-rsig_lp;
 
-% % clean the cardiac signal
-% hsig_fft=fft(hsig_lp);
-% hsig_fft(1:40)=0; % eliminate the noise
-% hsig_lp=real(ifft(hsig_fft));
+% clean the cardiac signal
+hsig_fft=fft(hsig_lp);
+% eliminate the noise
+hsig_fft(1:40)=0; % !! for 24 s !!
+hsig_lp=real(ifft(hsig_fft));
 
 figure('Name','SEPARATION OF BREATHING & CARDIAC SIGNAL');
 subplot(2,1,1)
